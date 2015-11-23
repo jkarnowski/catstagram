8919f668dbcabb8415201f3b2173417725d3cbfd
jkarnowski <jaclyn.karnowski@gmail.com>
2015-11-22 17:13:26 -0800

    create controller for cats app

--- app.js
+++ app.js
@@ -3,22 +3,54 @@
 //Open localhost:8000 in your browser.
 
 //Create an angular app called catstagram
-var app = ... catstagram
+var app = angular.module('catstagram', [])
 
 //Create a CatController
-app.controller('CatController', ['$scope', function($scope){
-	//Get the cats data using $http from data.json.
-	//On success function assign the data.cats to $scope.cats variable. 
-	// Don't forget to add $http as your contollers dependecy.
+app.controller('CatController', ['$scope', '$http', function($scope, $http){
 
+	$http.get('data.json')
+	.success(function(data){
+
+		$scope.cats = data.cats
+	})
+	.error(function(data){
+		console.log("failing")
+	})
 	//Write a $scope.like function that takes index as an argument and adds 1 to likes attribute of the cat on $scope.cats[index]. 
-	// $scope.like = ...
+
+	$scope.like = function(index){
+		$scope.cats[index].likes += 1
+	}
 
 	//Create a function that takes a newCat object as argument.
 	// Add likes attribute to the newCat object and assign 1 as the value.
 	// Add newCat to the beginning of the $scope.cats array.
 	// $scope.addCat = ...
+
+	$scope.addCat = function(newCat){
+		var myNewCat = newCat;
+		myNewCat.likes = 1;
+
+		$scope.cats.unshift(myNewCat);
+
+	};
+
+	$scope.comments = [
+	{
+		"message": "such a cutie"
+	},
+	{
+		"message": "adorbs"
+	},
+	{
+		"message": "fluffer nutter"
+	}
+	]
+
+	$scope.submitComment = function(string){
+	}
 }]);
+
 
 //Optional 
 
@@ -27,3 +59,10 @@ app.controller('CatController', ['$scope', function($scope){
 
 //Optional
 //Create the submitComments function in your controller
+
+
+
+// REFACTOR AND MAKE A FACTORY!!!
+// app.factory('catFactory', '$http' [function($http){
+
+// }])
